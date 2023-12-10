fec6e070ea07eefe5b5d59d11bceea680b8b81b2
MY-PC <MY-PC@192.168.1.3>
2023-12-10 15:10:13 +0200

    StringCreation program that reads from the user 2 string values called S1 and S2. The program should then form and print out a new String called S that is composed of the first half of S2,followed  by the second half of S1,then the second half of S2,which in turn is
    followed by the first half of S1.The program should force the user to
    enter S1 and S2 strings that have an even number of characters.

--- test/src/gitpack/StringCreation.java
+++ test/src/gitpack/StringCreation.java
@@ -14,15 +14,17 @@ System.out.print("Enter the first string : ");
 String S1=sc.nextLine();
 System.out.print("Enter the second string : ");
 String S2=sc.nextLine();
-
+//S1 and S1 must have an even number of characters.
 if(S1.length()%2!=0 || S2.length()%2!=0) {
 	System.out.println("Strings must have an even number of characters");
 	System.exit(0);
 }
+
 String S3=S2.substring(0, S2.length()/2);
 String S4=S1.substring(S1.length()/2, S1.length());
 String S5=S2.substring((S2.length()/2), S2.length());
 String S6=S1.substring(0, (S1.length()/2));
+//Display new String S.
 System.out.print("S:"+S3+S4+S5+S6);
 	}
 
