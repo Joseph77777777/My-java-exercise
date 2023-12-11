57e78a2d7ba9235fdfbde329bfb1e3cd81a75511
MY-PC <MY-PC@192.168.1.3>
2023-12-11 15:42:34 +0200

    Program which reads speed and angle from the user and then prints out the distance travelled by a ball.

---
+++ test/src/gitpack/Projectile.java
@@ -0,0 +1,38 @@
+/*@author Joseph Nakhle 
+
+
+@version: 2023-03 (4.27.0)
+@since release 12/9/23 
+*/
+
+package gitpack;
+import java.util.Scanner;
+import java.util.*;
+
+public class Projectile {
+	//method that convert angle from degrees to radiant.
+public static double angleRad(double angledeg) {
+	double Rad=angledeg*(Math.PI/180);
+	return Rad;
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the initial Speed in m/s:");
+int V0=sc.nextInt();
+System.out.println("Enter the initial angle in degrees :");
+double a=sc.nextDouble();
+
+final double G= 9.8;
+//Equation of the distance.
+double d=((Math.pow(V0, 2)*Math.sin(2*angleRad(a)))/G);
+
+System.out.print("The distance travelled is : ");
+//format the output to 2 significant digits.
+System.out.printf("%,.1f",d );
+System.out.print(" meters");
+
+	}
+
+}
